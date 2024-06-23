d4f56b37516629c3d5f2336ea8aee2406b9475a3
RonitZinzuvadiya1040 <ronitzinzuvadiya2001@gmail.com>
2024-06-23 17:27:05 +0530

    compare

---
+++ 01_Basics/04_comperision.js
@@ -0,0 +1,55 @@
+let score = 'ronit'
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "Ronit" => true
+
+let someNumber = 33;
+
+
+let StringNumber = String(someNumber);
+
+// console.log(StringNumber);
+// console.log(typeof(StringNumber)); // string
+
+
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
