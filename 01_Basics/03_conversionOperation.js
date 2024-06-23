fad07585b63a0aeb250235c6c3e148fb93760d25
RonitZinzuvadiya1040 <ronitzinzuvadiya2001@gmail.com>
2024-06-23 14:22:46 +0530

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = 'ronit'
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "Ronit" => true
+
+let someNumber = 33;
+
+
+let StringNumber = String(someNumber);
+
+console.log(StringNumber);
+console.log(typeof(StringNumber)); // string
