9cf294c2f3ea89eae50baa765e15bddd01456656
RonitZinzuvadiya1040 <ronitzinzuvadiya2001@gmail.com>
2024-06-23 14:00:00 +0530

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3+3); // we are using node js not browser.
+
+// console.log(3+3);
+// console.log(3
+//    +3); // code readability shoul be high
+
+// console.log('Ronit');
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53 Range
+// bigint = for large numbers
+// string = "assembly of characters"
+// boolean => true or false
+
+// Null = stand alone value (type = object)
+// undefined = kindda place holder for a value which is not defined yet (type = undefined)
+// symbol = uniquness
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
